da15ceac01a4981ccbdcd9e63354e53d6374c362
STUDENTS\student <e.amanollahi@gmail.com>
2017-06-07 14:59:22 -0400

    * Mile Stone1 (Assignment)

---
+++ milestone1/milestone1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace milestone1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+            int id;
+            bool didItWork = false;
+             //returns bool                     //out: is a refrence , ID must have  a value that is being sent, you need to send the value
+            didItWork = int.TryParse(Console.ReadLine(), out id);
+
+            if (didItWork == true)
+            {
+                Console.WriteLine("Id is : " + id);
+            }
+            else
+
+            {
+                Console.WriteLine("Error, Id is not an integer.");
+
+            }
+            Console.ReadLine();
+        }
+        // if this == to false >>>> not an integer  or try parse worked but the id is less than 0
+    }
+}
